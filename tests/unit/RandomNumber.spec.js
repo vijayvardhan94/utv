f4743b407ba230bf7d7d403ef1b93448512f589c
vijayvardhan94 <tadimetv@oregonstate.edu>
2023-08-08 20:34:12 -0400

    skeletal test

---
+++ tests/unit/RandomNumber.spec.js
@@ -0,0 +1,8 @@
+import RandomNumber from "@/components/RandomNumber";
+import { mount } from "@vue/test-utils";
+
+describe("RandomNumber", () => {
+  test("RandomNumber should be 0 if the button is not clicked", () => {
+    expect(true).toBe(true);
+  });
+});
