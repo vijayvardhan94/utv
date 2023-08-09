708050e21c32e395ffac65d7a6ea4fdcfc54380d
vijayvardhan94 <tadimetv@oregonstate.edu>
2023-08-08 20:04:17 -0400

    added test component and initial test

---
+++ tests/unit/AppHeader.spec.js
@@ -0,0 +1,11 @@
+import AppHeader from "@/components/AppHeader";
+
+describe("AppHeader", () => {
+  test("If user is not logged in do not show logout button", () => {
+    expect(true).toBe(true);
+  });
+
+  test("If user is logged in how logout button", () => {
+    expect(true).toBe(true);
+  });
+});
