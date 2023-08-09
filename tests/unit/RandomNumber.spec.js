9722b7474fe4d3ab8bf0c5964d514c219cb4e907
vijayvardhan94 <tadimetv@oregonstate.edu>
2023-08-08 20:57:12 -0400

    more tests

--- tests/unit/RandomNumber.spec.js
+++ tests/unit/RandomNumber.spec.js
@@ -3,6 +3,31 @@ import { mount } from "@vue/test-utils";
 
 describe("RandomNumber", () => {
   test("RandomNumber should be 0 if the button is not clicked", () => {
+    const wrapper = mount(RandomNumber);
+    expect(wrapper.html()).toContain("<span>0</span>");
     expect(true).toBe(true);
+  });
+
+  test("RandomNumber should be between 1 and 10 if the button is clicked", async () => {
+    const wrapper = mount(RandomNumber);
+    wrapper.find("button").trigger("click");
+    await wrapper.vm.$nextTick();
+    const randomNumber = parseInt(wrapper.find("span").element.textContent);
+    expect(randomNumber).toBeGreaterThanOrEqual(1);
+    expect(randomNumber).toBeLessThanOrEqual(10);
+  });
+
+  test("RandomNumber should be between 200 and 300 if the button is clicked", async () => {
+    const wrapper = mount(RandomNumber, {
+      propsData: {
+        min: 200,
+        max: 300,
+      },
+    });
+    wrapper.find("button").trigger("click");
+    await wrapper.vm.$nextTick();
+    const randomNumber = parseInt(wrapper.find("span").element.textContent);
+    expect(randomNumber).toBeGreaterThanOrEqual(200);
+    expect(randomNumber).toBeLessThanOrEqual(300);
   });
 });
