8d79527bd9ba63b704d2b54b6152b55c17af1280
vijayvardhan94 <tadimetv@oregonstate.edu>
2023-08-08 20:21:15 -0400

    make test async

--- tests/unit/AppHeader.spec.js
+++ tests/unit/AppHeader.spec.js
@@ -1,11 +1,18 @@
 import AppHeader from "@/components/AppHeader";
+import { mount } from '@vue/test-utils';
 
 describe("AppHeader", () => {
   test("If user is not logged in do not show logout button", () => {
-    expect(true).toBe(true);
+    const wrapper = mount(AppHeader);
+    expect(wrapper.find("button").isVisible()).toBe(false);
   });
 
-  test("If user is logged in how logout button", () => {
-    expect(true).toBe(true);
+  test("If user is logged in show logout button", async () => {
+    const wrapper = mount(AppHeader);
+    wrapper.setData({ loggedIn: true });
+    //because the assertion happens before it is registered with the dom. So wait,
+    //for the dopm updates. This is done via async await.
+    await wrapper.vm.$nextTick();
+    expect(wrapper.find("button").isVisible()).toBe(true);
   });
 });
